2ea33364ba5dcb8bf2fd075e40df1c999b57e231
lzryz <78732957+lzryz@users.noreply.github.com>
2023-10-17 22:11:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from pytube import YouTube
+import time
+from tqdm import tqdm
 link = input('>> ')
+for i in tqdm(range(100)):
+    time.sleep(0.015)
+from pytube import YouTube
 y = YouTube(link)
 s = y.streams.get_highest_resolution()
 print(s.default_filename)
